1a3ba559a6009537cd9beaa31a42d3daea216e1a
Dedalo101 <dedalosmith@proton.me>
2025-10-21 14:45:14 +0000

    🧹 Clean slate: Remove all unnecessary files

--- pages/index.js
+++
@@ -1,1 +0,0 @@
-export default function Home() { return <div><h1>TECHNO Generator</h1><p>Use the Electron app in /electron-app folder</p></div>; }
--- public/run-techno-generator.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "🎵 Starting TECHNO Generator..."
-cd "$(dirname "$0")"
-chmod +x TECHNO-Generator-linux-x64/TECHNO-Generator
-./TECHNO-Generator-linux-x64/TECHNO-Generator
